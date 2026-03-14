6233502b7b38b2efef1949252a9b360958b8c75d
hezek-02 <nery.ezequiel.capo@fing.edu.uy>
2026-03-13 21:56:15 -0300

    backup

--- practico1/ejercicio2.cpp
+++ practico1/ejercicio2.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
-int main() {
-    //cout << sizeof(int) << std::endl;
-    //64 MB = 64 × 1024 × 1024 bytes (LLC) 2^26 -> (2^7* 2^2) * (2^7* 2^2) * (2^7* 2^2) = 2^27 
-    const int DIM = 128;
-    
-    vector<vector<vector<int>>> matrix1(DIM, vector<vector<int>>(DIM, vector<int>(DIM)));
-    vector<vector<vector<int>>> matrix2(DIM, vector<vector<int>>(DIM, vector<int>(DIM)));
-    vector<vector<vector<int>>> matrix3(DIM, vector<vector<int>>(DIM, vector<int>(DIM)));
-    
-    cout << "3 3D matrixes created successfully" << std::endl;
-    
-    return 0;
+void matrix_mult(float *A, float *B, float *C, int N, int BS) {
+    int i,j,k,ii,jj,kk;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    for (ii = 0; ii < N; ii += BS)
+        for (jj = 0; jj < N; jj += BS)
+            for (kk = 0; kk < N; kk += BS)
+            
+                for (i = ii; i < ii+BS; i++)
+                    for (j = jj; j < jj+BS; j++)
+                        for (k = kk; k < kk+BS; k++)
+                            C[i*N+j] += A[i*N+k] * B[k*N+j];
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Matrix multiplication time: " << elapsed.count() << " seconds" << endl;
 }
+
+int main() {
+    cout << sizeof(float) << std::endl;
+    //64 MB = 64 × 1024 × 1024 bytes (LLC) 2^26 -> (2^2) * 2^13 ¨* 2^13
+    const int DIM = (1 << 13); // 2^13 = 8192
+    
+    vector<vector<float>> matrixA(DIM, vector<float>(DIM));
+    vector<vector<float>> matrixB(DIM, vector<float>(DIM));
+    vector<vector<float>> matrixC(DIM, vector<float>(DIM));
+    
+    int BS = 32; // 2^5
+    matrix_mult(&matrixA[0][0], &matrixB[0][0], &matrixC[0][0], DIM, BS);
+    
+    // for (int BS = 32; BS < (1 << 13); BS *= 4 ) {
+    //     matrix_mult(&matrixA[0][0], &matrixB[0][0], &matrixC[0][0], DIM, BS);
+    // }                  
+    return 0;
+    
+}
+
