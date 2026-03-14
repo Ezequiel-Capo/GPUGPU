21135ebe21f19eee8f4587acc99d30c43d6aa4b0
hezek-02 <nery.ezequiel.capo@fing.edu.uy>
2026-03-13 22:13:43 -0300

    version2

--- practico1/ejercicio2.cpp
+++ practico1/ejercicio2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <chrono>
 
 using namespace std;
@@ -12,10 +12,10 @@ void matrix_mult(float *A, float *B, float *C, int N, int BS) {
     for (ii = 0; ii < N; ii += BS)
         for (jj = 0; jj < N; jj += BS)
             for (kk = 0; kk < N; kk += BS)
-            
-                for (i = ii; i < ii+BS; i++)
-                    for (j = jj; j < jj+BS; j++)
-                        for (k = kk; k < kk+BS; k++)
+
+                for (i = ii; i < min(ii + BS, N); i++)
+                    for (j = jj; j < min(jj + BS, N); j++)
+                        for (k = kk; k < min(kk + BS, N); k++)
                             C[i*N+j] += A[i*N+k] * B[k*N+j];
 
     auto end = chrono::high_resolution_clock::now();
@@ -24,20 +24,26 @@ void matrix_mult(float *A, float *B, float *C, int N, int BS) {
 }
 
 int main() {
-    cout << sizeof(float) << std::endl;
+    //cout << sizeof(float) << std::endl;
     //64 MB = 64 × 1024 × 1024 bytes (LLC) 2^26 -> (2^2) * 2^13 ¨* 2^13
     const int DIM = (1 << 13); // 2^13 = 8192
-    
-    vector<vector<float>> matrixA(DIM, vector<float>(DIM));
-    vector<vector<float>> matrixB(DIM, vector<float>(DIM));
-    vector<vector<float>> matrixC(DIM, vector<float>(DIM));
+    const size_t TOTAL = static_cast<size_t>(DIM) * DIM;
+
+    float* matrixA = new float[TOTAL]();
+    float* matrixB = new float[TOTAL]();
+    float* matrixC = new float[TOTAL]();
     
     int BS = 32; // 2^5
-    matrix_mult(&matrixA[0][0], &matrixB[0][0], &matrixC[0][0], DIM, BS);
+    matrix_mult(matrixA, matrixB, matrixC, DIM, BS);
     
     // for (int BS = 32; BS < (1 << 13); BS *= 4 ) {
-    //     matrix_mult(&matrixA[0][0], &matrixB[0][0], &matrixC[0][0], DIM, BS);
+    //     matrix_mult(matrixA, matrixB, matrixC, DIM, BS);
     // }                  
+
+    delete[] matrixA;
+    delete[] matrixB;
+    delete[] matrixC;
+
     return 0;
     
 }
